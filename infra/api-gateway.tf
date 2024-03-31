7970fdbba95bcde52613f8ced5df302d2ae80f04
GHBAlbuquerque <ghb.albuquerque@gmail.com>
2024-03-31 19:38:03 -0300

    feature: cria pipeline de deploy

---
+++ infra/api-gateway.tf
@@ -0,0 +1,70 @@
+resource "aws_api_gateway_rest_api" "api_gateway_fiap_postech" {
+  name        = var.project_name
+  description = "created with terraform"
+
+  endpoint_configuration {
+    types = ["REGIONAL"]
+  }
+}
+
+resource "aws_api_gateway_resource" "proxy" {
+  rest_api_id = aws_api_gateway_rest_api.api_gateway_fiap_postech.id
+  parent_id   = aws_api_gateway_rest_api.api_gateway_fiap_postech.root_resource_id
+  path_part   = "hello"
+}
+
+resource "aws_api_gateway_method" "proxy" {
+  rest_api_id   = aws_api_gateway_rest_api.api_gateway_fiap_postech.id
+  resource_id   = aws_api_gateway_resource.proxy.id
+  http_method   = "GET"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "lambda" {
+  rest_api_id = aws_api_gateway_rest_api.api_gateway_fiap_postech.id
+  resource_id = aws_api_gateway_method.proxy.resource_id
+  http_method = aws_api_gateway_method.proxy.http_method
+
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = "arn:aws:apigateway:${var.region}:lambda:path/2015-03-31/functions/${var.lambda_arn}:$${stageVariables.stage}/invocations"
+} //TODO
+
+resource "aws_api_gateway_deployment" "deployment_dev" {
+  depends_on = [
+    aws_api_gateway_integration.lambda
+  ]
+
+  rest_api_id = aws_api_gateway_rest_api.api_gateway_fiap_postech.id
+}
+  
+
+resource "aws_api_gateway_stage" "dev" {
+  deployment_id = aws_api_gateway_deployment.deployment_dev.id
+  rest_api_id   = aws_api_gateway_rest_api.api_gateway_fiap_postech.id
+  stage_name    = "dev"
+
+  variables = {
+    "stage" = "dev"
+  }
+}
+
+output "api_gateway_id" {
+  value = aws_api_gateway_rest_api.api_gateway_fiap_postech.id
+}
+
+output "hello_dev" {
+  value = "${aws_api_gateway_stage.dev.invoke_url}/hello"
+}
+
+
+resource "aws_lambda_permission" "lambda_permission" {
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = var.lambda_name
+  principal     = "apigateway.amazonaws.com"
+
+  # The "/*/*" portion grants access from any method on any resource
+  # within the API Gateway REST API.
+  source_arn = "${aws_api_gateway_rest_api.api_gateway_fiap_postech.execution_arn}/*/*"
+}
---
+++ infra/variables.tf
@@ -0,0 +1,20 @@
+variable "project_name" {
+  default = "fiap-postech-fastfood"
+}
+
+variable "region" {
+  default = "us-east-1"
+}
+
+variable "profile" {
+  default = "default"
+}
+
+variable "lambda_name" {
+  default = "postech-lambda-auth-fastfood"
+}
+
+variable "lambda_arn" {
+  default = ""
+}
+
