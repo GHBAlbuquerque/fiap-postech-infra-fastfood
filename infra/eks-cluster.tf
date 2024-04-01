0720a2b0e7bc50ce2906cdc65dc6a869accbe8f5
GHBAlbuquerque <ghb.albuquerque@gmail.com>
2024-04-01 20:59:27 -0300

    infra: adiciona criacao do cluster eks e node group

---
+++ infra/eks-cluster.tf
@@ -0,0 +1,49 @@
+resource "aws_eks_cluster" "eks_cluster_fiap_postech" {
+  name     = "eks_cluster_fiap_postech"
+  role_arn = var.lab_role_arn
+
+  vpc_config {
+    subnet_ids = [var.subnet_id_a, var.subnet_id_b, var.subnet_id_c, var.subnet_id_d]
+  }
+
+  # Ensure that IAM Role permissions are created before and deleted after EKS Cluster handling.
+  # Otherwise, EKS will not be able to properly delete EKS managed EC2 infrastructure such as Security Groups.
+#  depends_on = [
+#    aws_iam_role_policy_attachment.example-AmazonEKSClusterPolicy,
+#    aws_iam_role_policy_attachment.example-AmazonEKSVPCResourceController,
+#  ]
+}
+
+output "endpoint" {
+  value = aws_eks_cluster.eks_cluster_fiap_postech.endpoint
+}
+
+output "kubeconfig-certificate-authority-data" {
+  value = aws_eks_cluster.eks_cluster_fiap_postech.certificate_authority[0].data
+}
+
+resource "aws_eks_node_group" "eks_node_group_fiap_postech" {
+  cluster_name    = aws_eks_cluster.eks_cluster_fiap_postech.name
+  node_group_name = "example"
+  node_role_arn   = var.lab_role_arn
+  subnet_ids      = [var.subnet_id_a, var.subnet_id_b, var.subnet_id_c, var.subnet_id_d]
+  instance_types = ["t3.medium"]
+
+  scaling_config {
+    desired_size = 1
+    max_size     = 3
+    min_size     = 1
+  }
+
+  update_config {
+    max_unavailable = 1
+  }
+
+  # Ensure that IAM Role permissions are created before and deleted after EKS Node Group handling.
+  # Otherwise, EKS will not be able to properly delete EC2 Instances and Elastic Network Interfaces.
+#  depends_on = [
+#    aws_iam_role_policy_attachment.example-AmazonEKSWorkerNodePolicy,
+#    aws_iam_role_policy_attachment.example-AmazonEKS_CNI_Policy,
+#    aws_iam_role_policy_attachment.example-AmazonEC2ContainerRegistryReadOnly,
+#  ]
+}
--- infra/variables.tf
+++ infra/variables.tf
@@ -18,3 +18,26 @@ variable "lambda_arn" {
   default = "arn:aws:lambda:us-east-1:211125478754:function:postech-lambda-auth-fastfood"
 }
 
+variable "lab_role_arn" {
+  default = "arn:aws:iam::211125478754:role/LabRole"
+}
+
+variable "subnet_id_a" {
+  deafult = "subnet-0a9a5b3113f41772a"
+}
+
+variable "subnet_id_b" {
+  deafult = "subnet-023c0926286854cc1"
+}
+
+variable "subnet_id_c" {
+  deafult = "subnet-06bfe0041e39d1b38"
+}
+
+variable "subnet_id_d" {
+  deafult = "subnet-00decacbc2f6540ed"
+}
+
+variable "subnet_id_e" {
+  deafult = "subnet-0cc96aea146f03bc4"
+}
