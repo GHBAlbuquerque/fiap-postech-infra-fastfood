bed61c652f8680a36bd5d30790f9d86fe05a9325
GHBAlbuquerque <ghb.albuquerque@gmail.com>
2024-04-22 21:45:15 -0300

    feature: edita metodo de identidade

--- infra/api-gateway.tf
+++ infra/api-gateway.tf
@@ -62,6 +62,8 @@ resource "aws_api_gateway_authorizer" "lambda_authorizer_cognito" {
   rest_api_id            = aws_api_gateway_rest_api.api_gateway_fiap_postech.id
   authorizer_uri         = "arn:aws:apigateway:${var.region}:lambda:path/2024-04-22/functions/${var.lambda_arn}/invocations"
   authorizer_credentials = var.lab_role_arn
+  type                   = REQUEST
+  identity_source        = "method.request.header.cpf_cliente"
 }
 
 
