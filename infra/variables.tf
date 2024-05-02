f49077cb118803d88aa737cae820af061f47c195
GHBAlbuquerque <ghb.albuquerque@gmail.com>
2024-05-02 20:47:30 -0300

    infra: ajuste de ids de vpc e subnets

--- infra/variables.tf
+++ infra/variables.tf
@@ -15,37 +15,37 @@ variable "lambda_name" {
 }
 
 variable "lambda_arn" {
-  default = "arn:aws:lambda:us-east-1:211125478754:function:postech-lambda-auth-fastfood"
+  default = "arn:aws:lambda:us-east-1:067184771399:function:postech-lambda-auth-fastfood"
 }
 
 variable "lab_role_arn" {
-  default = "arn:aws:iam::211125478754:role/LabRole"
+  default = "arn:aws:iam::067184771399:role/LabRole"
 }
 
 variable "vpc_id" {
-  default = "vpc-07dec253c1b5a77ea"
+  default = "vpc-0c33afcdadaa4be7b"
 }
 
 variable "subnet_id_a" {
-  default = "subnet-0226b059f9bb3e97b"
+  default = "subnet-092106de434121bca"
 }
 
 variable "subnet_id_b" {
-  default = "subnet-094d8c1602642bf95"
+  default = "subnet-021d5fd5534bb9fbd"
 }
 
 variable "subnet_id_c" {
-  default = "subnet-02a64d0abe9230131"
+  default = "subnet-05685364d9e045913"
 }
 
 variable "subnet_id_d" {
-  default = "subnet-096ff3a74878067c5"
+  default = "subnet-066ce0c3304da4788"
 }
 
 variable "subnet_id_e" {
-  default = "subnet-044446105935a3cd7"
+  default = "subnet-0bdae3752039a482f"
 }
 
-variable "load_balancer_eks_dns" {
-  default = "a07e02eddfe6d479e91fa2b99d4be427-516352852.us-east-1.elb.amazonaws.com"
-}
+//variable "load_balancer_eks_dns" {
+//  default = "a07e02eddfe6d479e91fa2b99d4be427-516352852.us-east-1.elb.amazonaws.com"
+//}
