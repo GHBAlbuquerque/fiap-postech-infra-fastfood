7676e02b26509a195bb5281ca2e2c2794f4dd59c
GHBAlbuquerque <ghb.albuquerque@gmail.com>
2024-05-03 15:30:42 -0300

    infra: aumenta interval do healthcheck para 120s

--- infra/application-load-balancer.tf
+++ infra/application-load-balancer.tf
@@ -20,6 +20,7 @@ resource "aws_lb_target_group" "target-group-cluster-fiap" {
     path    = "/actuator/health"
     port    = 30007
     matcher = "200"
+    interval = 120
     timeout = 60
   }
 }
