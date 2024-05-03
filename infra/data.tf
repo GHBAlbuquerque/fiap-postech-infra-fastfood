008b8be8ea762adc6526e72c436a2ef080e3ba62
GHBAlbuquerque <ghb.albuquerque@gmail.com>
2024-05-03 14:23:01 -0300

    infra: volta node group name

--- infra/data.tf
+++ infra/data.tf
@@ -1,8 +1,7 @@
 data "aws_instance" "ec2" {
   depends_on = [aws_eks_cluster.eks_cluster_fiap_postech]
   filter {
-    #name   = "tag:eks:eks_node_group_fiap_postech"
-    name   = "tag:eks:nodegroup-name"
+    name   = "tag:eks:eks_node_group_fiap_postech"
     values = ["NG-nginx"]
   }
 }
