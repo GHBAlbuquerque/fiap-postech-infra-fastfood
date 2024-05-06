61b6934282b66b1907698ca728f3f88b98e04474
GHBAlbuquerque <ghb.albuquerque@gmail.com>
2024-05-06 15:16:26 -0300

    infra: adiciona name preffix no sg

--- infra/security-group.tf
+++ infra/security-group.tf
@@ -1,4 +1,5 @@
 resource "aws_security_group" "sg" {
+  name_prefix = "sec-group-cluster-fiap"
   name        = "sec-group-cluster-fiap"
   description = "Seucirty Group for EKS Cluster"
   vpc_id      = var.vpc_id
