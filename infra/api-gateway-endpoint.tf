aa5b68cac3a5318caf1aad22de7a91fd85199e4c
GHBAlbuquerque <ghb.albuquerque@gmail.com>
2024-05-06 21:57:53 -0300

    fix: remove / errada

--- infra/api-gateway-endpoint.tf
+++ infra/api-gateway-endpoint.tf
@@ -1,5 +1,5 @@
 // GET - PRODUCTS
-/resource "aws_api_gateway_resource" "endpoint_get_products" {
+resource "aws_api_gateway_resource" "endpoint_get_products" {
   rest_api_id = aws_api_gateway_rest_api.api_gateway_fiap_postech.id
   parent_id   = aws_api_gateway_rest_api.api_gateway_fiap_postech.root_resource_id
   path_part   = "products"
