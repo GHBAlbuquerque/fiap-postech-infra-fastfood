da983fb59b398e7f61f9f25d49f58f9aa4f7b43b
GHBAlbuquerque <ghb.albuquerque@gmail.com>
2024-05-13 09:48:08 -0300

    infra: usa arquivo tfvars para variaveis

--- infra/variables.tf
+++ infra/variables.tf
@@ -19,39 +19,39 @@ variable "lambda_name" {
 }
 
 variable "lambda_arn" {
-  default = "arn:aws:lambda:us-east-1:067184771399:function:postech-lambda-auth-fastfood"
+  default = "lambda_arn"
 }
 
 variable "lab_role_arn" {
-  default = "arn:aws:iam::067184771399:role/LabRole"
+  default = "labrole_arn"
 }
 
 variable "vpc_id" {
-  default = "vpc-0c33afcdadaa4be7b"
+  default = "vpc-id"
 }
 
 variable "vpc_cidr_block" {
-  default = "172.31.0.0/16"
+  default = "0.0.0.0/0"
 }
 
 variable "subnet_id_a" {
-  default = "subnet-092106de434121bca"
+  default = "subnet-a"
 }
 
 variable "subnet_id_b" {
-  default = "subnet-021d5fd5534bb9fbd"
+  default = "subnet-b"
 }
 
 variable "subnet_id_c" {
-  default = "subnet-05685364d9e045913"
+  default = "subnet-c"
 }
 
 variable "subnet_id_d" {
-  default = "subnet-066ce0c3304da4788"
+  default = "subnet-d"
 }
 
 variable "subnet_id_e" {
-  default = "subnet-0bdae3752039a482f"
+  default = "subnet-e"
 }
 
 variable "instance_type" {
